0d24cd4113f234db092714d2e5eab03fd7480db2
Pavel Tiunov <pavel.tiunov@gmail.com>
2020-10-16 13:25:11 -0700

    Revert default MySQL dialect

--- rust/datafusion/src/sql/parser.rs
+++ rust/datafusion/src/sql/parser.rs
@@ -22,7 +22,7 @@
 use sqlparser::ast::ObjectName;
 use sqlparser::{
     ast::{ColumnDef, Statement as SQLStatement, TableConstraint},
-    dialect::{keywords::Keyword, Dialect},
+    dialect::{keywords::Keyword, Dialect, GenericDialect},
     parser::{Parser, ParserError},
     tokenizer::{Token, Tokenizer},
 };
@@ -89,35 +89,10 @@ pub struct DFParser {
     parser: Parser,
 }
 
-#[derive(Debug)]
-/// MySql dialect that supports '`' for escaping
-pub struct MySqlDialectWithBackTicks {}
-
-impl Dialect for MySqlDialectWithBackTicks {
-    fn is_delimited_identifier_start(&self, ch: char) -> bool {
-        ch == '"' || ch == '`'
-    }
-
-    fn is_identifier_start(&self, ch: char) -> bool {
-        // See https://dev.mysql.com/doc/refman/8.0/en/identifiers.html.
-        // We don't yet support identifiers beginning with numbers, as that
-        // makes it hard to distinguish numeric literals.
-        (ch >= 'a' && ch <= 'z')
-            || (ch >= 'A' && ch <= 'Z')
-            || ch == '_'
-            || ch == '$'
-            || (ch >= '\u{0080}' && ch <= '\u{ffff}')
-    }
-
-    fn is_identifier_part(&self, ch: char) -> bool {
-        self.is_identifier_start(ch) || (ch >= '0' && ch <= '9')
-    }
-}
-
 impl DFParser {
     /// Parse the specified tokens
     pub fn new(sql: &str) -> Result<Self, ParserError> {
-        let dialect = &MySqlDialectWithBackTicks {};
+        let dialect = &GenericDialect {};
         DFParser::new_with_dialect(sql, dialect)
     }
 
@@ -135,7 +110,7 @@ impl DFParser {
 
     /// Parse a SQL statement and produce a set of statements with dialect
     pub fn parse_sql(sql: &str) -> Result<Vec<Statement>, ParserError> {
-        let dialect = &MySqlDialectWithBackTicks {};
+        let dialect = &GenericDialect {};
         DFParser::parse_sql_with_dialect(sql, dialect)
     }
 
