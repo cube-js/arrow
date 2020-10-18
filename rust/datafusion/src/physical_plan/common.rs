4aefb497490d652c4f8cc04329c9afd9df67c024
Pavel Tiunov <pavel.tiunov@gmail.com>
2020-10-18 11:26:20 -0700

    Fix missed nanosecond conversion

--- rust/datafusion/src/physical_plan/common.rs
+++ rust/datafusion/src/physical_plan/common.rs
@@ -30,7 +30,7 @@ use array::{
     Int8Array, LargeStringArray, StringArray, UInt16Array, UInt32Array, UInt64Array,
     UInt8Array,
 };
-use arrow::array::TimestampMicrosecondArray;
+use arrow::array::{TimestampMicrosecondArray, TimestampNanosecondArray};
 use arrow::datatypes::{DataType, SchemaRef, TimeUnit};
 use arrow::error::Result as ArrowResult;
 use arrow::record_batch::RecordBatch;
@@ -163,6 +163,10 @@ pub fn create_batch_empty(schema: &Schema) -> ArrowResult<RecordBatch> {
             }
             DataType::Timestamp(TimeUnit::Microsecond, None) => Ok(Arc::new(
                 TimestampMicrosecondArray::from(vec![] as Vec<i64>),
+            )
+                as ArrayRef),
+            DataType::Timestamp(TimeUnit::Nanosecond, None) => Ok(Arc::new(
+                TimestampNanosecondArray::from_vec(vec![] as Vec<i64>, None),
             )
                 as ArrayRef),
             _ => Err(DataFusionError::NotImplemented(format!(
