795f4c87096aa179d5070085bd4cadb12de611a6
Pavel Tiunov <pavel.tiunov@gmail.com>
2021-01-07 18:43:30 -0800

    Remove debug output

--- rust/datafusion/src/physical_plan/merge_join.rs
+++ rust/datafusion/src/physical_plan/merge_join.rs
@@ -270,14 +270,14 @@ impl Stream for MergeJoinStream {
         let right = right_state
             .map(|(_, batch)| Ok(batch))
             .unwrap_or_else(|| self.right.empty_batch())?;
-        println!(
-            "Join on left (last: {}) at {}: {:?}",
-            self.left.is_last, left_cursor, left
-        );
-        println!(
-            "Join on right (last: {}) at {}: {:?}",
-            self.right.is_last, right_cursor, right
-        );
+        // println!(
+        //     "Join on left (last: {}) at {}: {:?}",
+        //     self.left.is_last, left_cursor, left
+        // );
+        // println!(
+        //     "Join on right (last: {}) at {}: {:?}",
+        //     self.right.is_last, right_cursor, right
+        // );
         let merge_result = merge_join(
             self.schema.clone(),
             &left,
@@ -290,7 +290,7 @@ impl Stream for MergeJoinStream {
             right_cursor,
             &self.join_type,
         );
-        println!("Join result: {:?}", merge_result);
+        // println!("Join result: {:?}", merge_result);
 
         Poll::Ready(Some(merge_result.map(
             |(
